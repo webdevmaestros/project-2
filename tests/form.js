c6b25858ad3bfbb259dd092307af7ff35c37d9e6
Angela Petrone <apetrone@hawk.iit.edu>
2022-04-05 17:21:10 -0500

    Write basic tests for all pages

--- tests/form.js
+++ tests/form.js
@@ -10,9 +10,48 @@ describe ('Landing Page', function() {
     it('if the form is empty it is not submittable', function(browser) {
       browser
         .url('http://localhost:8080/')
-        .click('button.submit')
+        .click('input.submit')
         .assert
           .urlEquals('http://localhost:8080/');
+    });
+
+});
+
+
+describe ('Billing Page', function() {
+
+    it('a form should exist on the page', function(browser) {
+      browser
+        .url('http://localhost:8080/billing/')
+        .assert
+          .elementPresent('form[name="billing-form"]');
+    });
+
+    it('if the form is empty it is not submittable', function(browser) {
+      browser
+        .url('http://localhost:8080/billing/')
+        .click('input.submit')
+        .assert
+          .urlEquals('http://localhost:8080/billing/');
+    });
+
+});
+
+describe ('Payment Page', function() {
+
+    it('a form should exist on the page', function(browser) {
+      browser
+        .url('http://localhost:8080/payment/')
+        .assert
+          .elementPresent('form[name="payment-form"]');
+    });
+
+    it('if the form is empty it is not submittable', function(browser) {
+      browser
+        .url('http://localhost:8080/payment/')
+        .click('input.submit')
+        .assert
+          .urlEquals('http://localhost:8080/payment/');
     });
 
 });
