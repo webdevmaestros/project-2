3e594db24f6a51404ebaff003757dcfe4d2d3409
avgelix <89323046+avgelix@users.noreply.github.com>
2022-04-06 21:30:46 -0500

    Initiate nightwatch and run tests (#14)
    * Initialize default package.json
    
    * Add Nightwatch, chromedriver and geckodriver deps
    
    * Ignore nightwatch cruft
    
    * Add nightwatch deafault config file
    
    * Place nightwatch tests into tests/
    
    * Default to chrome on nightwatch
    
    * Add a name to the shipping form and a class to the button on the landing page, to facilitate nightwatch testing
    
    * Add a forms.js file with basic nightwatch tests
    
    * Ignore cruft resulting from running nightwatch tests
    
    * Ignore cruft resulting from running nightwatch tests
    
    * Add specific name to forms
    
    * Write basic tests for all pages

---
+++ tests/form.js
@@ -0,0 +1,57 @@
+describe ('Landing Page', function() {
+
+    it('a form should exist on the page', function(browser) {
+      browser
+        .url('http://localhost:8080/')
+        .assert
+          .elementPresent('form[name="shipping-form"]');
+    });
+
+    it('if the form is empty it is not submittable', function(browser) {
+      browser
+        .url('http://localhost:8080/')
+        .click('input.submit')
+        .assert
+          .urlEquals('http://localhost:8080/');
+    });
+
+});
+
+
+describe ('Billing Page', function() {
+
+    it('a form should exist on the page', function(browser) {
+      browser
+        .url('http://localhost:8080/billing/')
+        .assert
+          .elementPresent('form[name="billing-form"]');
+    });
+
+    it('if the form is empty it is not submittable', function(browser) {
+      browser
+        .url('http://localhost:8080/billing/')
+        .click('input.submit')
+        .assert
+          .urlEquals('http://localhost:8080/billing/');
+    });
+
+});
+
+describe ('Payment Page', function() {
+
+    it('a form should exist on the page', function(browser) {
+      browser
+        .url('http://localhost:8080/payment/')
+        .assert
+          .elementPresent('form[name="payment-form"]');
+    });
+
+    it('if the form is empty it is not submittable', function(browser) {
+      browser
+        .url('http://localhost:8080/payment/')
+        .click('input.submit')
+        .assert
+          .urlEquals('http://localhost:8080/payment/');
+    });
+
+});
